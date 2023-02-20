0f5c37b96537442e98eae2f1f97c4b5c2ec47bab
Sat Garcia <sat@sandiego.edu>
2023-02-20 13:04:56 -0800

    initial version of file

---
+++ comp120_lab04.py
@@ -0,0 +1,32 @@
+"""
+Module: comp120_lab04
+
+Practice using recursion to deal with nested lists.
+"""
+
+def uniques(obj: Union[int, list]) -> list[int]:
+    """Returns a non-nested list of unique integers in <obj> (i.e. list with
+    no repeats).
+
+    >>> uniques(5)
+    [5]
+    """
+    if isinstance(obj, int):
+        # base case
+        return [obj]
+    else:
+        # recursive case
+        s = []
+        for item in obj:
+            s.extend(uniques(item))
+        return s
+
+
+def nested_list_contains(obj: Union[int, list]) -> bool:
+    """Return whether the given item appears in <objL.
+
+    Note that if <obj> is an integer, this function checks whether <item> is
+    equal to <obj>.
+    """
+
+    pass # remove this line and replace it with your implementation
